97f0426e75c842d3cbe73aab7060422c463e183e
Atarashiku <blcorbin@gmail.com>
2021-07-31 11:51:38 -0500

    initial commit

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+# Here is a comment for my program.
+print "This is a statement" # and here is a comment
---
+++ ex25.py
@@ -0,0 +1,11 @@
+def break_words(stuff):
+    """This function will break up words for us."""
+    words = stuff.split(' ')
+    return words
+def sort_words(words):
+    """Sort the words."""
+    return sorted(words)
+
+def print_first_word(words):
+    """Print the first word after popping it off."""
+    
